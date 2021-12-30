0da7a6b8e6a43062585f415f2d57f8f4ff793468
ryan <rsheehan@gmail.com>
2021-12-29 23:01:27 -0600

    changed wording around for affiliate links disclaimer

--- src/lib/components/recommended-products.svelte
+++ src/lib/components/recommended-products.svelte
@@ -8,12 +8,8 @@
 <h5>Recommended products<br/>to make this recipe</h5>
 <Carousel {products} />
 <div>
-  <p>By clicking on our ads and affiliate links, you are helping to support this blog.
-    We may make a small commission on products that are purchased through our links.
-    This allows us to continue to bring recipes and nutrition information from our home to yours
-    at no additional cost to you. A portion of all proceeds will go to benefit the JDRF and 
-    T1International.  #insulin4all</p><p>Thank you for your support! Click here to learn more about 
-    our Affiliate Program Memberships</p>  
+  <p>By clicking on our ads and affiliate links, you are helping to support this blog. We may make a small commission on products that are purchased through our links, at no additional cost to you. This allows us to continue to bring recipes and nutrition information from our home to yours. A portion of all proceeds will go to benefit the JDRF and  T1International.  #insulin4all</p>
+  <p>Thank you for your support! Click here to learn more about our Affiliate Program Memberships</p>  
 </div>
 
 
